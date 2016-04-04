34108beaad0fc181136fd6b7da17a39f1e742eea
Jarory13 <Jarory13@gmail.com>
2016-04-04 17:48:34 -0400

    Created player and enemy objects. Put enemies in allEnemies array via for loop.

--- finalProject/js/app.js
+++ finalProject/js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+ 
     this.sprite = 'images/enemy-bug.png';
+
+
 };
 
 // Update the enemy's position, required method for game
@@ -17,19 +20,61 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+
+Enemy.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function () {
+
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
+//create enemies from a 0 index and add them to the allEnemies array.
+for(var i = 0; i < 3; i++) {
+
+    allEnemies['enemy' + i] =  new Enemy();
+};
+
+for (var i = 0; i < allEnemies.length; i++) {
+    allEnemies[i].render();
+};
+
+
+var player = new Player();
+
+console.log(allEnemies, player);
+
+function getClickPosition(e) {
+    var xPosition = e.clientX;
+    var yPosition = e.clientY;
+}
+
+var mouseClick = getClickPosition
+
+console.log(mouseClick);
 
 
 // This listens for key presses and sends the keys to your
